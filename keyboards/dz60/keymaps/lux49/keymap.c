6a0b33c26e5603d338449756108809f7968e8c4f
Lucy <lucy@asagi.moe>
2021-09-19 02:46:36 +0200

    switching to neo-qwertz required me to clear up the caps lock key. now the tab key acts as the layer 1 button if pressed and hold and sends a normal KC_TAB when tapped once

--- keyboards/dz60/keymaps/lux49/keymap.c
+++ keyboards/dz60/keymaps/lux49/keymap.c
@@ -3,14 +3,14 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [0] = LAYOUT_60_iso_5x1u_split_rshift( 
     KC_ESC,        KC_1,    KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,     KC_MINS, KC_EQL,  KC_BSPC,      
-    KC_TAB,        KC_Q,    KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,     KC_LBRC, KC_RBRC,               
-    MO(1),         KC_A,    KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT, KC_NUHS,  KC_ENT,      
+    LT(1, KC_TAB),        KC_Q,    KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,     KC_LBRC, KC_RBRC,               
+    KC_CAPS, KC_A,    KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT, KC_NUHS,  KC_ENT,      
     OSM(MOD_LSFT), KC_NUBS, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM, KC_DOT, KC_SLSH, OSM(MOD_RSFT),  TT(1), 
     KC_LCTL,       KC_LGUI, KC_LALT,                   KC_SPC,                       KC_RALT, KC_RGUI, KC_RCTL, MO(3), TG(2)),
  [1] = LAYOUT_60_iso_5x1u_split_rshift( 
     KC_GRV,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_DEL,       
     _______, XXXXXXX, LCTL(KC_RGHT), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, XXXXXXX, XXXXXXX, KC_PSTE, KC_PSCR, KC_INS,               
-    _______, KC_CAPS, XXXXXXX, KC_PGDN, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_HOME, KC_END, XXXXXXX, _______,        
+    _______, XXXXXXX, XXXXXXX, KC_PGDN, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_HOME, KC_END, XXXXXXX, _______,        
     _______, XXXXXXX, KC_COPY, KC_CUT, XXXXXXX, XXXXXXX, LCTL(KC_LEFT), XXXXXXX, KC_APP, KC_VOLU, KC_VOLD, KC_MUTE, _______, _______, 
     _______, _______, _______, _______, _______, _______, _______, _______, _______),
  [2] = LAYOUT_60_iso_5x1u_split_rshift( 
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, KC_SPC,  _______, _______, _______, _______, _______),
  [4] = LAYOUT_60_iso_5x1u_split_rshift( 
    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+   KC_TAB,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
    KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_RSFT, _______,
    _______, XXXXXXX, _______, _______, _______, XXXXXXX, _______, _______, _______)
